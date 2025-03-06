fd8b398b6aae40e33406487580e5ae4740a75d8b
santiagodiaz21 <fsantiagodiazc21@gmail.com>
2025-03-06 11:53:40 -0500

    public

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("Public"))
 app.use("/api/factura", factura);
 app.use("/api/clientes", clientes);
 app.use("/api/items", items);
